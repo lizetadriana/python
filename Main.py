c7123d8987090a0771f9c5859d8bce95131c3684
lizetadriana <adriana.lizet.mata.cano@gmail.com>
2023-07-17 12:47:22 -0600

    Condicion

--- Main.py
+++ Main.py
@@ -1,3 +1,7 @@
 print("Hola mundo)")
 nombre=input("Ingresa tu nombre ")
 edad=int(input("Ingresa tu edad "))
+if(edad>=18):
+    print(nombre," eres mayor de edad")
+else:
+    print(f"{nombre} eres menor de edad")
